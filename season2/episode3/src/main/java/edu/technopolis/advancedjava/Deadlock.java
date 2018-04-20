68910146670640eca2615417ffe4dc131110b3d6
nikita <kobalt31@mail.ru>
2018-04-20 16:12:55 +0300

    add deadlock

--- season2/episode3/src/main/java/edu/technopolis/advancedjava/Deadlock.java
+++ season2/episode3/src/main/java/edu/technopolis/advancedjava/Deadlock.java
@@ -3,6 +3,8 @@ package edu.technopolis.advancedjava;
 public class Deadlock {
     private static final Object FIRST_LOCK = new Object();
     private static final Object SECOND_LOCK = new Object();
+    private static final Object t = new Object();
+    static int state = 0;
 
     public static void main(String[] args) throws Exception {
         Thread ft = new Thread(Deadlock::first);
@@ -16,7 +18,17 @@ public class Deadlock {
 
     private static void first() {
         synchronized(FIRST_LOCK) {
-            //insert some code here to guarantee a deadlock
+            synchronized (t) {
+                state++;
+                while(state < 2) {
+                    try {
+                        t.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                t.notifyAll();
+            }
             synchronized(SECOND_LOCK) {
                 //unreachable point
             }
@@ -31,7 +43,17 @@ public class Deadlock {
         }
         //reverse order of monitors
         synchronized(SECOND_LOCK) {
-            //insert some code here to guarantee a deadlock
+            synchronized (t) {
+                state++;
+                while(state < 2) {
+                    try {
+                        t.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                t.notifyAll();
+            }
             synchronized(FIRST_LOCK) {
                 //unreachable point
             }
